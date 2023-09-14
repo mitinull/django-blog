3141ca6b90b6e98f0654398072ba6b42de566077
mohammadT9030 <torabi9030@gmail.com>
2023-09-14 14:54:44 +0330

    Adding and Changing anchor tags and urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home-page"),
-    path("posts", views.all_posts, name="all-posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page"),
+    path("<slug:slug>", views.post_detail, name="post-detail-page"),
 ]
