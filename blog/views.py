f9443f6b3f07459bf44072e0969d6f6c57308171
mohammadT9030 <torabi9030@gmail.com>
2023-09-15 15:45:21 +0330

    Adding post-detail page and it's styles

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,5 @@ def home(request):
     return render(request, "blog/index.html")
 
 
-def all_posts(request):
-    return HttpResponse("All Posts")
-
-
 def post_detail(request, slug):
-    return HttpResponse("Post Detail")
+    return render(request, "blog/post-detail.html")
