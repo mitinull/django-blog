1a5fe22a7652f98eb848e4bd474be0245c2132fd
mohammadT9030 <torabi9030@gmail.com>
2023-10-05 14:04:00 +0330

    removing image field form photographer + adding posts

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,6 @@ from django.core.validators import MinLengthValidator
 
 class Photographer(models.Model):
     name = models.CharField(max_length=100)
-    image = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return self.name
